698f469ae27e22e9f665f14466ca77545295cda3
rohith-reddy-16 <21131a0543@gvpce.ac.in>
2024-06-22 22:56:57 +0530

    Update .gitignore and config.json, add user registration and login functionality

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
+const jwt=require("jsonwebtoken");
+
+const middleware=require("./middleware");
 const configJSON = require("./config.json");
 const user = require("./schema/usermodel");
 const app = express();
@@ -10,24 +14,30 @@ mongoose.connect(configJSON.mongodbURI).then(() => {
 
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.send("Welcome to Vrikshyan");
 });
 
 app.post("/register", async (req, res) => {
     try {
-        let obj = req.body;
-        const exist= await user.findOne({ email: obj.email });
-        if(exist){
-            return res.status(400).send("User Already Exists");
-        }
-        if(obj.password !== obj.confirmpassword){
-            return res.status(400).send("Password and Confirm Password do not match");
-        }
-        let newUser = new user(obj);
-        await newUser.save();
-        return res.status(201).send({state: "User Registered", value:newUser});
-
+        let arr = req.body;
+        let registeredUsers=[];
+        arr.map(async (obj) => {
+            const exist = await user.findOne({ email: obj.email });
+            if (exist) {
+                return res.status(400).send("User Already Exists");
+            }
+            if (obj.password !== obj.confirmpassword) {
+                return res.status(400).send("Password and Confirm Password do not match");
+            }
+            let newUser = new user(obj);
+            await newUser.save();
+            registeredUsers.push(newUser);
+            console.log(registeredUsers);
+        });
+        return res.status(201).send(registeredUsers);
     }
     catch (error) {
         console.log(error);
@@ -35,30 +45,71 @@ app.post("/register", async (req, res) => {
     }
 });
 
-app.get("/login", async(req, res)=>{
-    try{
-        let obj=req.body;
-        const exist= await user.findOne({ email: obj.email });
-        if(!exist){
+app.post("/login", async (req, res) => {
+    try {
+        let obj = req.body;
+        const exist = await user.findOne({ email: obj.email });
+        if (!exist) {
             return res.status(400).send("User does not exist");
         }
-        if(obj.password !== exist.password){
+        if (obj.password !== exist.password) {
             return res.status(400).send("Password is incorrect");
         }
-        return res.status(200).send("User Logged In");
+        let payload={
+            user:{
+                id:exist.id
+            }
+        }
+        jwt.sign(payload,configJSON.jwtSecret,{expiresIn:3600000},(err,token)=>{
+            if(err) throw err;
+            return res.json({token});
+        });
+
+        // return res.status(200).send("User Logged In");
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         return res.status(500).send("Server Error");
     }
 });
 
-app.get("/users", async(req, res)=>{
-    try{
-        const users= await user.find();
+app.get("/users",middleware,  async (req, res) => {
+    try {
+        const users = await user.find();
         return res.status(200).send(users);
     }
-    catch(error){
+    catch (error) {
+        console.log(error);
+        return res.status(500).send("Server Error");
+    }
+});
+
+app.get("/user/:id",middleware, async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const foundUser = await user.findById(userId);
+        if (!foundUser) {
+            return res.status(404).send("User not found");
+        }
+        return res.status(200).send(foundUser);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Server Error");
+    }
+});
+
+app.delete("/user/:id",middleware, async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const deletedUser = await user.findByIdAndDelete(userId);
+        //delete user using email
+        // const userEmail = req.params.email;
+        // const deletedUser = await user.findOneAndDelete({ email: userEmail });
+        if (!deletedUser) {
+            return res.status(404).send("User not found");
+        }
+        return res.status(200).send("User deleted successfully");
+    } catch (error) {
         console.log(error);
         return res.status(500).send("Server Error");
     }
@@ -66,5 +117,5 @@ app.get("/users", async(req, res)=>{
 
 
 app.listen(configJSON.port, () => {
-    console.log("Server is running on port 5000");
+    console.log("Server is running on port http://localhost:" + configJSON.port);
 });
