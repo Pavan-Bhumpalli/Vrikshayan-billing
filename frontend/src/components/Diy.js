2ae6c3ca0a1db061040148d894c1d51be4aa6197
dwijeshbing <dwijeshbingumalla@gmail.com>
2024-06-28 00:07:59 +0530

    Ui changes made

--- frontend/src/components/Diy.js
+++ frontend/src/components/Diy.js
@@ -6,6 +6,7 @@ import { UseContextProvider } from "./contexts/StepperContext";
 import Account from "./steps/Account";
 import Details from "./steps/Details";
 import FinalDiy from "./steps/FinalDiy";
+import { FaPaintBrush } from 'react-icons/fa';
 
 const Diy = () => {
     const [currentStep, setCurrentStep] = useState(1);
@@ -27,9 +28,9 @@ const Diy = () => {
             case 1:
                 return <Account setPk={setPk} next={handleNext} />;
             case 2:
-                return <Details pk={pk} next={handleNext}/>;
+                return <Details pk={pk} next={handleNext} />;
             case 3:
-                return <FinalDiy Customer_pk={pk}/>;
+                return <FinalDiy Customer_pk={pk} />;
             default:
                 return null;
         }
@@ -47,7 +48,11 @@ const Diy = () => {
         <div className="flex items-center h-screen">
             <Sidebar />
             <div className="w-[95%] mx-auto pl-72">
-                <div className="pb-2 bg-white shadow-xl rounded-2xl">
+                <div className="flex flex-col items-center justify-center pb-2 bg-white shadow-xl rounded-2xl">
+                    <div className={`mt-8 mb-8 rounded-full h-12 w-12 flex flex-col items-center justify-center bg-green-600 text-white font-bold  border-green-600"}`}>
+                        <span className="text-xl font-bold text-white"><FaPaintBrush className='w-7 h-7' /></span>
+                        <div className='absolute w-32 mt-16 text-xl font-bold text-center text-[#2e7120] uppercase'>DIY</div>
+                    </div>
                     <div className="container mt-5 horizontal">
                         <Stepper steps={steps} currentStep={currentStep} />
                         <div className="p-10 my-10">
