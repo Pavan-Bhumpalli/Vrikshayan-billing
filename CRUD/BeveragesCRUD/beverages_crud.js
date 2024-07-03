8a16f31514936683a5d64bb1b50ad5095aa787b8
rohith-reddy-16 <21131a0543@gvpce.ac.in>
2024-07-04 00:12:57 +0530

    chore: Update schema and routes for admin activities
    The schema and routes for admin activities related to nursery, DIY, beverages, and farm produce have been updated. This allows the admin to perform CRUD operations on these items.

--- CRUD/BeveragesCRUD/beverages_crud.js
+++ CRUD/BeveragesCRUD/beverages_crud.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const StartFuncCreateBeverages = require("../../routes/Create/createBeverages");
 const StartFuncGetBeverages = require("../../routes/Get/getBeveragesItems");
+const StartFuncDeleteBeverageByPk = require("../../routes/Delete/deleteBeverageByPk");
 
 const router = express.Router();
 
 router.post("/createBeverages", StartFuncCreateBeverages);
 router.get("/getBeverages", StartFuncGetBeverages);
+router.delete("/beverage/:pk", StartFuncDeleteBeverageByPk);
 
 module.exports = router;
--- frontend/src/components/Admin/AdminActivities.js
+++ frontend/src/components/Admin/AdminActivities.js
@@ -2,91 +2,113 @@ import React, { useState, useEffect } from 'react';
 import AdminSidebar from './AdminSidebar';
 import axios from 'axios';
 import { MdAdd } from "react-icons/md";
-import { FaEye } from "react-icons/fa6";
 import swal from 'sweetalert2';
+import { MdDeleteOutline } from "react-icons/md";
 
-const AdminActivities = ({ type, inp }) => {
-  const [data, setData] = useState([]);
+const AdminActivities = ({ type, inp, del }) => {
+    const [data, setData] = useState([]);
 
-  const loadUsers = async () => {
-    const response = await axios.get(`http://localhost:5000/${type}`);
-    setData(response.data);
-  };
+    const loadUsers = async () => {
+        const response = await axios.get(`http://localhost:5000/${type}`);
+        setData(response.data);
+    };
 
-  useEffect(() => {
-    loadUsers();
-  }, [type]);
+    useEffect(() => {
+        loadUsers();
+    }, [type]);
 
-  const addNewItem = async () => {
-    const { value: formValues } = await swal.fire({
-      title: 'Add New Item',
-      html: `
+    const addNewItem = async () => {
+        const { value: formValues } = await swal.fire({
+            title: 'Add New Item',
+            html: `
         <input type='text' id='name' class='swal2-input' placeholder='Name'>
         <input type='number' id='cost' class='swal2-input' placeholder='Cost'>
       `,
-      focusConfirm: false,
-      showCancelButton: true,
-      confirmButtonText: 'Add Item',
-      preConfirm: () => {
-        return {
-          name: document.getElementById('name').value,
-          cost: document.getElementById('cost').value
+            focusConfirm: false,
+            showCancelButton: true,
+            confirmButtonText: 'Add Item',
+            preConfirm: () => {
+                return {
+                    name: document.getElementById('name').value,
+                    cost: document.getElementById('cost').value
+                }
+            }
+        });
+
+        if (formValues) {
+            try {
+                const response = await axios.post(`http://localhost:5000/${inp}`, formValues);
+                setData(prevData => [...prevData, response.data]);
+                swal.fire('Added!', 'Your item has been added.', 'success');
+            } catch (error) {
+                swal.fire('Error', 'There was a problem adding your item.', 'error');
+            }
         }
-      }
-    });
+    };
 
-    if (formValues) {
-      try {
-        const response = await axios.post(`http://localhost:5000/${inp}`, formValues);
-        setData(prevData => [...prevData, response.data]);
-        swal.fire('Added!', 'Your item has been added.', 'success');
-      } catch (error) {
-        swal.fire('Error', 'There was a problem adding your item.', 'error');
-      }
-    }
-  };
+    const deleteItem = async (item_pk) => {
+        const result = await swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!'
+        });
+        if (result.isConfirmed) {
+            try {
+                const response = await axios.delete(`http://localhost:5000/${del}/${item_pk}`);
+                if (response.status === 200) {
+                    swal.fire('Deleted!', 'Your item has been deleted.', 'success');
+                    loadUsers();
+                }
+            } catch (error) {
+                swal.fire('Error', 'There was a problem deleting your item.', 'error');
+            }
+        }
+    };
 
-  return (
-    <div className='flex '>
-      <AdminSidebar />
-      <div className='flex flex-col w-full p-8'>
-        <div className='flex justify-end mb-4'>
-          <button onClick={addNewItem} className='flex items-center font-semibold bg-[#228b22] text-white px-4 py-2 rounded hover:bg-[#165816]'>
-            <MdAdd className='inline w-6 h-6 mr-2' />
-            <span>Create</span>
-          </button>
+    return (
+        <div className='flex '>
+            <AdminSidebar />
+            <div className='flex flex-col w-full p-8'>
+                <div className='flex justify-end mb-4'>
+                    <button onClick={addNewItem} className='flex items-center font-semibold bg-[#228b22] text-white px-4 py-2 rounded hover:bg-[#165816]'>
+                        <MdAdd className='inline w-6 h-6 mr-2' />
+                        <span>Create</span>
+                    </button>
+                </div>
+                <div className='overflow-x-auto'>
+                    <table className='min-w-full bg-white shadow-md rounded'>
+                        <thead className='bg-gray-200'>
+                            <tr>
+                                <th className='py-2 px-4 border'>Pk</th>
+                                <th className='py-2 px-4 border'>Name</th>
+                                <th className='py-2 px-4 border'>Cost</th>
+                                <th className='py-2 px-4 border'>Edit</th>
+                                <th className='py-2 px-4 border'>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((item) => (
+                                <tr key={item._id} className='text-center border-t'>
+                                    <td className='py-2 px-4 border'>{item.nursery_pk || item.item_pk || item.beverageId || item.produce_pk || item.diy_pk}</td>
+                                    <td className='py-2 px-4 border'>{item.name}</td>
+                                    <td className='py-2 px-4 border'>{item.cost || item.price}</td>
+                                    <td className='py-2 px-4 border'>
+                                        <button className='text-blue-500 hover:underline'>Edit</button>
+                                    </td>
+                                    <td className='py-2 px-4 border'>
+                                        <button onClick={() => deleteItem(item.nursery_pk || item.item_pk || item.beverageId || item.produce_pk || item.diy_pk)} className='text-red-500 hover:underline'><MdDeleteOutline size={25} /></button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <div className='overflow-x-auto'>
-          <table className='min-w-full bg-white shadow-md rounded'>
-            <thead className='bg-gray-200'>
-              <tr>
-                <th className='py-2 px-4 border'>Pk</th>
-                <th className='py-2 px-4 border'>Name</th>
-                <th className='py-2 px-4 border'>Cost</th>
-                <th className='py-2 px-4 border'>Edit</th>
-                <th className='py-2 px-4 border'>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr key={item._id} className='text-center border-t'>
-                  <td className='py-2 px-4 border'>{item.nursery_pk || item.item_pk || item.beverageId || item.produce_pk || item.diy_pk}</td>
-                  <td className='py-2 px-4 border'>{item.name}</td>
-                  <td className='py-2 px-4 border'>{item.cost || item.price}</td>
-                  <td className='py-2 px-4 border'>
-                    <button className='text-blue-500 hover:underline'>Edit</button>
-                  </td>
-                  <td className='py-2 px-4 border'>
-                    <button className='text-red-500 hover:underline'>Delete</button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AdminActivities;
